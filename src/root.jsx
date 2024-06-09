2bc1f0d0b1abd67aa17e26529031525d98e72605
Occccn <big.island.yz@gmail.com>
2024-06-09 12:15:51 +0900

    ignore tsx

--- src/root.jsx
+++ src/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Tmp from './tmp';
+import App from './App.jsx';
+import Tmp from './tmp.jsx';
 
 function Root() {
   return (
