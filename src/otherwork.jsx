d9153e41c1784150c814c28f2a60feb7fb448df4
y_y <yuki@MacBook-Air.AirPort>
2024-06-21 18:19:51 +0900

    add comments

--- src/otherwork.jsx
+++ src/otherwork.jsx
@@ -19,10 +19,10 @@ const Card = ({ title, description,url}) => (
   
   const Otherwork = () => {
     const cardsData = [
-      { title: "楽曲提供", description: "Description for Card 1",url:'https://www.youtube.com/embed/baP9jWS8R2k?si=WINbtOIs9X5U-Z3i'},
-      { title: "ドラム", description: "Description for Card 2",url:"https://www.youtube.com/embed/bjuZztOTtDw?si=XCk8of2aFyxqNFXp"},
-      { title: "ボーカル", description: "Description for Card 3",url:"https://www.youtube.com/embed/0frST4k5HSM?si=yhfI-upWxVoXMFxE" },
-      { title: "ボーカロイドカバー", description: "Description for Card 4",url:"https://www.youtube.com/embed/3P7XlNa18A8?si=uGLEEyEuqrXCwSBw"}
+      { title: "楽曲提供", description: "ひまわり / 市ノ瀬悠和　※作曲・編曲・Mixを担当",url:'https://www.youtube.com/embed/baP9jWS8R2k?si=WINbtOIs9X5U-Z3i'},
+      { title: "ドラム", description: "Num-Ami-Dabutz（NUMBER GIRL） drum cover",url:"https://www.youtube.com/embed/bjuZztOTtDw?si=XCk8of2aFyxqNFXp"},
+      { title: "ボーカル", description: "食べなくちゃ（衝動的の人） cover",url:"https://www.youtube.com/embed/0frST4k5HSM?si=yhfI-upWxVoXMFxE" },
+      { title: "ボーカロイドカバー", description: "MONKEY（group_inou） 初音ミク cover",url:"https://www.youtube.com/embed/3P7XlNa18A8?si=uGLEEyEuqrXCwSBw"}
     ];
     return (
         <>
