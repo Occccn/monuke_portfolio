604261ef1ef08a71940b99949492943a2cc6f18a
Occccn <big.island.yz@gmail.com>
2025-07-07 09:45:33 +0900

    headerに関するアニメーション以外のcssをtailwindへ移植

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,24 +12,29 @@ const Header = ({ title }) => {
 
   return (
     <>
-      <header className="fixed top-0 left-0 w-full z-10 bg-[#0f9e7f] h-[120px] shadow-md flex items-center">
+      <header className="fixed top-0 left-0 w-full z-10 bg-[#0f9e7f] h-[120px] shadow-lg flex items-center">
         <div className="header-title flex-grow">
           <h2 className="subtitle text-left m-0 text-white text-[20px] ml-[10%]">
             monuke
           </h2>
-          <h1 className="text-[2em] leading-[1.1] font-bold">{title}</h1>
+          <h1 className="text-left m-0 text-transparent text-[40px] ml-[10%] mt-[10px] w-1/2 relative overflow-hidden whitespace-nowrap font-bold leading-[1.1]">
+            {title}
+          </h1>
         </div>
-        <nav className="header-nav">
+        <nav className="header-nav hidden md:block">
           <ul className="flex gap-[10px] text-[20px] list-none p-0">
             <li>
-              <Link to="/" className="text-[#f4f4f4] py-[5px] px-[10px]">
+              <Link
+                to="/"
+                className="text-[#f4f4f4] py-[5px] px-[10px] no-underline hover:text-[#333] transition-colors duration-300"
+              >
                 Home
               </Link>
             </li>
             <li>
               <Link
                 to="/Vocaloid"
-                className="text-[#f4f4f4] py-[5px] px-[10px]"
+                className="text-[#f4f4f4] py-[5px] px-[10px] no-underline hover:text-[#333] transition-colors duration-300"
               >
                 Vocaloid Original
               </Link>
@@ -38,7 +43,7 @@ const Header = ({ title }) => {
             <li>
               <Link
                 to="/Otherwork"
-                className="text-[#f4f4f4] py-[5px] px-[10px]"
+                className="text-[#f4f4f4] py-[5px] px-[10px] no-underline hover:text-[#333] transition-colors duration-300"
               >
                 Other Works
               </Link>
