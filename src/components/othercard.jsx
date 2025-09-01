6f34c1f7c122e6ac9d77385551048da7791a1ec5
Occccn <big.island.yz@gmail.com>
2025-09-01 20:27:24 +0900

    update

--- src/components/othercard.jsx
+++ src/components/othercard.jsx
@@ -4,14 +4,15 @@ import "../assets/card.css"; // CSSファイルをインポート
 function OtherCard({ url, title, description }) {
   return (
     <div className="card">
-      <h2 className="card-title-other">{title}</h2>
-      <div className="video-responsive">
+      <h2 className="self-start text-2xl mb-2.5 mt-0">{title}</h2>
+      <div className="relative pb-[56.25%] h-0">
         <iframe
           src={url}
           loading="lazy"
           // frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
+          className="absolute top-0 left-0 w-full h-full border-none"
         ></iframe>
       </div>
       <div className="card-content">
--- src/components/videocard.jsx
+++ src/components/videocard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import "../assets/card.css"; // CSSファイルをインポート
+import "../assets/card.css"; // CSSファイルをインポート
 import FullScreenText from "./textdetail.jsx";
 function VideoCard({
   url,
