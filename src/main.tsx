60bcf7d2666bbdfaea37df9ee24505231075b75c
Occccn <big.island.yz@gmail.com>
2025-09-06 16:07:54 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
-// @ts-ignore
 import Root from "./root.jsx";
 const container = document.getElementById("root");
-// @ts-ignore
-const root = createRoot(container);
+const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
