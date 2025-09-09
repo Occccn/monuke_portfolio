972cc04e7f9e896c4d4fb69a24ef6dff8a11a855
Occccn <big.island.yz@gmail.com>
2025-09-09 10:55:52 +0900

    migrate otherwork component to TypeScript

---
+++ src/data/otherwork-content.ts
@@ -0,0 +1,28 @@
+interface OtherworkCard {
+  title: string;
+  description: string;
+  url: string;
+}
+
+export const otherworkCards: OtherworkCard[] = [
+  {
+    title: "Compose",
+    description: "ひまわり / 市ノ瀬悠和　※作曲・編曲・Mixを担当",
+    url: "https://www.youtube.com/embed/baP9jWS8R2k?si=WINbtOIs9X5U-Z3i",
+  },
+  {
+    title: "Drums",
+    description: "Num-Ami-Dabutz（NUMBER GIRL） drum cover",
+    url: "https://www.youtube.com/embed/bjuZztOTtDw?si=XCk8of2aFyxqNFXp",
+  },
+  {
+    title: "Vocal",
+    description: "食べなくちゃ（衝動的の人） cover",
+    url: "https://www.youtube.com/embed/0frST4k5HSM?si=yhfI-upWxVoXMFxE",
+  },
+  {
+    title: "Vocaloid Cover",
+    description: "MONKEY（group_inou） 初音ミク cover",
+    url: "https://www.youtube.com/embed/3P7XlNa18A8?si=uGLEEyEuqrXCwSBw",
+  },
+];
