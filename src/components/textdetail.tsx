ba2b86ca01c2c4bb30030ceed051127f2c795f28
Occccn <big.island.yz@gmail.com>
2025-09-10 09:11:54 +0900

    migrate textdetail component to TypeScript

---
+++ src/components/textdetail.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+interface FullScreenTextProps {
+  description_detail: string[];
+}
+
+const FullScreenText = ({ description_detail }: FullScreenTextProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button
+        className="cursor-pointer border-none py-2.5 px-5 text-base !bg-white text-[rgb(67,64,230)] transition-colors duration-300 hover:text-[rgba(65,62,241,0.3)]"
+        onClick={toggleMenu}
+      >
+        read more
+      </button>
+      {isOpen && (
+        <div className="fixed inset-0 w-full h-full text-white bg-black bg-opacity-90 flex justify-center items-center text-left z-50 whitespace-normal">
+          <div
+            className="absolute top-2.5 right-2.5 cursor-pointer text-2xl before:content-['×'] before:text-white before:text-4xl hover:before:text-gray-300"
+            onClick={toggleMenu}
+          ></div>
+          <div className="w-[70%]">
+            <ul className="list-none text-center">
+              {description_detail.map((item, index) => (
+                <li key={index} className="p-2.5">
+                  <span>{item}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default FullScreenText;
