90f5edcb9807ccc8e314b980f644fb1aa58b9f96
Occccn <big.island.yz@gmail.com>
2025-11-08 16:25:24 +0900

    add-react-helmet-async

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 
 import Root from "./root.jsx";
 const container = document.getElementById("root");
@@ -8,6 +9,8 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <Root />
+    <HelmetProvider>
+      <Root />
+    </HelmetProvider>
   </React.StrictMode>
 );
